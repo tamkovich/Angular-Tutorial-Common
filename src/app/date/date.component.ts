af36f080ad7310f6e26eacd4cd39232ec8378bec
Denis Tamkovich <tamkovichdenis@gmail.com>
2019-04-10 15:19:26 +0300

    4. Check Logged In with ngIf

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DateComponent implements OnInit {
   message: string;
   items = ["Django", "Flask", "Odoo", "AioHTTP", "Tornado"];
+  loggedIn = true;
 
   constructor() { 
   	setInterval(()=>{
